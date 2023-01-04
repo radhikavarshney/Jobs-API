cff79fe0a19e0dbdccb12c5ee7cb0b39409b7d71
radhikavarshney <rvarshney2001@gmail.com>
2023-01-04 11:43:44 +0530

    6th commit

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,11 +1,21 @@
+const Jobs = require('../models/Job')
+const {StatusCodes}= require('http-status-codes')
+const {NotFoundError,
+    BadRequestError}= require('../errors')
+
 const getAllJobs = async (req,res)=>{
-    res.send('get all jobs')
+   const jobs= await Jobs.find({createdBy:req.user.userId}).sort('createdAt')
+   res.status(StatusCodes.OK).json({jobs,count:jobs.length})
+
 }
 const getJobs = async (req,res)=>{
     res.send('get jobs')
 }
 const createJobs = async (req,res)=>{
-    res.send('createjobs')
+    req.body.createdBy = req.user.userId
+    const job = await Jobs.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+   
 }
 const updateJobs = async (req,res)=>{
     res.send('update jobs')
--- starter/models/User.js
+++ starter/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
      type:String,
      required:[true, 'Please provide email'],
      match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "please provide a valid email"],
-    //  unique:true,
+     unique:true,
     },
     password:{
         type:String,
@@ -30,7 +30,12 @@ userSchema.pre('save',async function(){
 })
 
 userSchema.methods.createJWT= function (){
-    return jwt.sign({userId:this._id,name:this.name},'jwtSecret',{expiresIn:"30d"})
+    return jwt.sign({userId:this._id,name:this.name},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+}
+
+userSchema.methods.comparePassword = async function (candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
 }
 
 module.exports = mongoose.model('User', userSchema)
